b1e9e433620eac5c68b50c02683fe26be0660e12
creep <creep.wei@gmail.com>
2015-02-05 17:54:12 +0800

    测试用例

---
+++ testlib/e.js
@@ -0,0 +1,8 @@
+define( './testlib/e', function ( require, exports, module ) {
+    var f = require( './testlib/f' );
+    var g = require( './testlib/g' );
+
+    module.exports = {
+        name: 'moduleE'
+    };
+} );
